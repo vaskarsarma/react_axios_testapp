4624724c52c9020177e3c65902e1ff391856e7c4
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-05-27 01:25:02 +0400

    Added post selection click event

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,29 +7,45 @@ import './Blog.css';
 import axios from "axios";
 
 class Blog extends Component {
-    state={
-        posts: []
+    state = {
+        posts: [],
+        selectedID: null
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response =>{
-                const posts= response.data.slice(0,4);
-                const updatedPosts=posts.map(post => {
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
                     return {
                         ...post,
-                        author:"Vaskar"
+                        author: "Vaskar"
                     }
                 });
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
                 //console.log(response);
             });
     }
 
-    render () {
-        const Posts=this.state.posts.map(post =>{
-            return <Post key={post.id} title={post.title} author={post.author} />
-        })
+    postClickHandler = (id) => {
+        // console.log(id);
+        // const posts =[...this.state.posts];
+        // const post = posts.find(p => p.id === id);
+        // this.setState({viewPost:post});
+        // console.log(post.title);
+        // console.log(post.body);
+        this.setState({selectedID:id});
+    }
+
+    render() {
+        const Posts = this.state.posts.map(post => {
+            return <Post 
+                        key={post.id} 
+                        id={post.id} 
+                        title={post.title} 
+                        author={post.author} 
+                        Clicked={()=>this.postClickHandler(post.id)} />
+        });
 
         return (
             <div>
@@ -37,7 +53,7 @@ class Blog extends Component {
                     {Posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedID} />
                 </section>
                 <section>
                     <NewPost />
