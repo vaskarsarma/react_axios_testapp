e18780c8ee4f01affe33961717430f70103f7865
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-05-30 08:02:25 +0400

    Added Link to stop page reload during navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import "./Blog.css";
 
@@ -15,10 +15,21 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                {/* <a href="/">Home</a> */}
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                {/*<a href="/new-post">New Post</a> */}
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </Link>
+                {/* <Link to="/new-post">New Post</Link> */}
               </li>
             </ul>
           </nav>
