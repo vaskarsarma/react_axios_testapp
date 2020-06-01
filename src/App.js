8f3294fb33865cba3fb755d7a021a6809c6c3800
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-06-01 07:16:08 +0400

    Usage of basename for base route handling

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
+      // <BrowserRouter basename='/posts'> //Using basename for base route handling
+      <BrowserRouter basename='/posts'>
+        <div className='App'>
           <Blog />
         </div>
       </BrowserRouter>
